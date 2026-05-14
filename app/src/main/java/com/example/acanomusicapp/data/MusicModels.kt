b126711738d30581655191b7b05a6f5a9992f076
Abraham <acv81505@lasallebajio.edu.mx>
2026-05-13 21:41:35 -0600

    Creacion de models y conexion API

---
+++ app/src/main/java/com/example/acanomusicapp/data/MusicModels.kt
@@ -0,0 +1,19 @@
+package com.example.acanomusicapp.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Album(
+    val id: Int,
+    val title: String,
+    val artist: String,
+    val description: String = "",
+    val image: String
+)
+
+sealed class NetworkResponse {
+    object Loading : NetworkResponse()
+    data class Success(val data: List<Album>) : NetworkResponse()
+    data class SuccessDetail(val album: Album) : NetworkResponse()
+    data class Error(val message: String) : NetworkResponse()
+}
